295288e1593a8263d03d65ba01e011c25ff78548
Livia Belao <livia-belao@aluno.senai.br>
2024-03-18 17:12:05 -0300

    feat: input data

--- src/screens/Mapas/index.jsx
+++ src/screens/Mapas/index.jsx
@@ -1,17 +1,22 @@
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, DateInput } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 export default function Mapas() {
   const [task, setTask] = useState("");
+  const [populacao, setPopulacao] = useState("");
+  const [data, setData] = useState("");
   const [tasks, setTasks] = useState([]);
 
   const addTask = () => {
     if (task.trim().length > 0) {
       setTasks([...tasks, { id: Math.random().toString(), value: task }]);
       setTask("");
+      setData("");
+      setPopulacao("")
     }
   };
+
   return (
     <View>
       <Text>Mapas</Text>
@@ -20,6 +25,18 @@ export default function Mapas() {
           placeholder="Digite o nome do Planeta"
           onChangeText={setTask}
           value={task}
+        />
+        <TextInput
+          placeholder="Digite a quantidade de população"
+          onChangeText={setPopulacao}
+          keyboardType="numeric"
+          value={populacao}
+        />
+        <TextInput
+          placeholder="Digite a quantidade de população"
+          onChangeText={setData}
+          secureTextEntry={true}
+          value={data}
         />
         <TouchableOpacity onPress={addTask}>
           <Text>Adicionar</Text>
