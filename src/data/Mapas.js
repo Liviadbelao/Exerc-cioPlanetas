5eaa1e7642c9df10bc1e1bd332874ff2a4d888df
Livia Belao <livia-belao@aluno.senai.br>
2024-03-18 16:22:28 -0300

    feat: dados mocados

---
+++ src/data/Mapas.js
@@ -0,0 +1,14 @@
+export const mapas = [
+    {
+        id: 1,
+        nome: "Plutão",
+        data: "04/11/2022",
+        corPlaneta: "Azul",
+        população: 600,
+        recusosNaturais: "Minerais",
+        nHumanos: 20,
+        localizacao: "Via Lactea",
+        comunicacao: "Radio",
+        governante:"Presidente"
+    }
+]
