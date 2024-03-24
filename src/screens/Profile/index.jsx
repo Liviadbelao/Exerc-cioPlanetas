c7e67c77431e01980fa835ea11f55e2cf59f4d49
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2024-03-24 01:06:30 -0300

    feat: editar e deletar

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -1,10 +1,20 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, TouchableOpacity } from "react-native";
+import Planetas from "../../models/Planetas";
+const listaPlanetas = new Planetas()
 export default function Profile({ route }) {
   const navigation = useNavigation();
   const { data } = route.params;
+  
+  const editUser = () => {
+    navigation.navigate("Form", { planeta: data, edit: true });
+  };
+
+  const deleteUser = () => {
+    listaPlanetas.remove(data.id);
+    navigation.navigate("Mapas");
+  };
 console.log(data);
   return (
     <View>
@@ -12,6 +22,14 @@ console.log(data);
       <Text>Nome: {data.nome}</Text>
       <Text>Natureza: {data.natureza}</Text>
       <Text>Data: {data.data}</Text>
+      <View >
+          <TouchableOpacity  onPress={editUser}>
+            <Text>Editar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={deleteUser}>
+            <Text>Excluir</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
